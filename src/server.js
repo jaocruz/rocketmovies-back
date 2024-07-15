3292156dc120fdd5b2f66a40cfda1b175eb3fe3f
João <mcruznq@gmail.com>
2024-07-15 09:56:53 -0300

    #

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+require("dotenv/config");
 require("express-async-errors");
+
 const express = require("express");
 
 const database = require("./database/sqlite");
@@ -36,5 +38,5 @@ app.use((error, request, response, next) => {
   })
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
